bdf5e0b36d812b255a48fb322fc3aabf135c9a3c
ve-edant <vedantsalunke17473@gmail.com>
2025-08-29 09:22:46 +0530

    action.ts_fixed

--- src/app/admin/_actions.ts
+++ src/app/admin/_actions.ts
@@ -2,35 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
-import { checkRole } from "@/utils/roles";
 
-export async function setRole(formData: FormData) {
-  if (!checkRole("ADMIN")) {
-    return { message: "Not Authorized" };
-  }
-
-  try {
-    const res = await clerkClient.users.updateUserMetadata(
-      formData.get("id") as string,
-      { publicMetadata: { role: formData.get("role") } }
-    );
-    return { message: res.publicMetadata };
-  } catch (err) {
-    return { message: err };
-  }
-}
-
-export async function removeRole(formData: FormData) {
-  try {
-    const res = await clerkClient.users.updateUserMetadata(
-      formData.get("id") as string,
-      { publicMetadata: { role: null } }
-    );
-    return { message: res.publicMetadata };
-  } catch (err) {
-    return { message: err };
-  }
-}
 
 export async function deleteUser(userId: string) {
   try {
